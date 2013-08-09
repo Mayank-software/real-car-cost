4e3d6b216a8e7771a7399350c5f86bff9aa2364f
Ajeesh Mohan <madaboutcode@gmail.com>
2013-08-09 14:55:41 +0530

    Adding bangalore swift price

--- data/data.js
+++ data/data.js
@@ -1,6 +1,6 @@
 var db = { }
 db.saved = { }
-db.saved['Swift VXI (Kerala)'] = {
+db.saved['Swift VXI/VDI (Kerala)'] = {
   "petrol_onroad": 579800.00,
   "petrol_service_cost": 3600,
   "petrol_mileage": 12.5,
@@ -8,11 +8,12 @@ db.saved['Swift VXI (Kerala)'] = {
   "diesel_service_cost": 4500,
   "diesel_mileage": 16,
 };
-db.saved['i10 (Kerala)'] = {
-  "petrol_onroad": 579800.00,
+
+db.saved['Swift VXI/VDI (Bangalore)'] = {
+  "petrol_onroad": 626234.00,
   "petrol_service_cost": 3600,
   "petrol_mileage": 12.5,
-  "diesel_onroad": 711037.00,
+  "diesel_onroad": 764741.00,
   "diesel_service_cost": 4500,
   "diesel_mileage": 16,
-}
+};
